be5fa6e4666819feeff45a02c33a71fc7fdbff82
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2020-09-27 13:59:01 +0530

    Add files via upload

---
+++ todoapp/todoapp/todofrontend/build/precache-manifest.8ff003fdcdfa2aa1c9ed54d75afef453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7251ee31dd9e4213af28c40cc0cd706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12bc47e5ef28cd871cea",
+    "url": "/static/css/main.97c744b1.chunk.css"
+  },
+  {
+    "revision": "957cc2b50c3410f375d5",
+    "url": "/static/js/2.0aeadd12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0aeadd12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bc47e5ef28cd871cea",
+    "url": "/static/js/main.4870c5ec.chunk.js"
+  },
+  {
+    "revision": "2295530d43d11a050ed6",
+    "url": "/static/js/runtime-main.a55446e7.js"
+  }
+]);
